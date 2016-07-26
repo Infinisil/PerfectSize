8af277f55f0ac5b79589f9956cfb10ef814dd2f0
Silvan Mosberger <infinisil@icloud.com>
2016-07-27 00:10:28 +0200

    Added more documentation

--- Source/Binary.swift
+++ Source/Binary.swift
@@ -8,11 +8,21 @@
 
 public protocol BinarySize : Size {}
 
+/**
+The base struct for building a struct of a certain binary size. Continously use the typealiases `O` and `I` to encode a number.
+
+**Examples**:
+```
+typealias FourtyTwo = B.I.0.I.0.I.0
+typealias TwoHundredFiftySix = B.I.O.O.O.O.O.O.O.O
+```
+*/
 public struct B : BinarySize {
 	/// Initialize this struct with every byte set to the given one
 	public init(byte: UInt8 = 0) {}
 }
 
+/// A struct with a size of the base struct times two.
 public struct Times2<S : BinarySize> : BinarySize {
 	let a, b : S
 	
@@ -23,6 +33,7 @@ public struct Times2<S : BinarySize> : BinarySize {
 	}
 }
 
+/// A struct with a size of the base struct plus one byte.
 public struct BinaryPlus1<S : BinarySize> : BinarySize {	
 	let a : S
 	let x : UInt8
@@ -35,6 +46,8 @@ public struct BinaryPlus1<S : BinarySize> : BinarySize {
 }
 
 public extension BinarySize {
+	/// Add the bit `0` to the resulting size of the type. This essentially multiplies the size by 2.
 	typealias O = Times2<Self>
+	/// Add the bit `1` to the resulting size of the type. This essentially multiplies the size by 2 and adds 1.
 	typealias I = BinaryPlus1<O>
 }
--- Source/Decimal.swift
+++ Source/Decimal.swift
@@ -9,11 +9,21 @@
 
 public protocol DecimalSize : Size {}
 
+/**
+The base struct for building a struct of a certain decimal size. Continously use the typealiases `n[0-9]` to encode a number.
+
+**Examples**:
+```
+typealias FourtyTwo = D.n4.n2
+typealias Hundred = D.n1.n0.n0
+```
+*/
 public struct D : DecimalSize {
 	/// Initialize this struct with every byte set to the given one
 	public init(byte: UInt8 = 0) {}
 }
 
+/// A struct with a size of the base struct plus one byte.
 public struct DecimalPlus1<S : DecimalSize> : DecimalSize {
 	let a : S
 	let x : UInt8
@@ -25,6 +35,7 @@ public struct DecimalPlus1<S : DecimalSize> : DecimalSize {
 	}
 }
 
+/// A struct with a size of the base struct times ten.
 public struct Times10<S : Size> : DecimalSize {
 	let a0, a1, a2, a3, a4, a5, a6, a7, a8, a9 : S
 	
@@ -44,14 +55,24 @@ public struct Times10<S : Size> : DecimalSize {
 }
 
 public extension DecimalSize {
+	/// Add the digit `0` to the resulting size of the type. This essentially multiplies the size by 10.
 	typealias n0 = Times10<Self>
+	/// Add the digit `1` to the resulting size of the type. This essentially multiplies the size by 10 and adds 1.
 	typealias n1 = DecimalPlus1<n0>
+	/// Add the digit `2` to the resulting size of the type. This essentially multiplies the size by 10 and adds 2.
 	typealias n2 = DecimalPlus1<n1>
+	/// Add the digit `3` to the resulting size of the type. This essentially multiplies the size by 10 and adds 3.
 	typealias n3 = DecimalPlus1<n2>
+	/// Add the digit `4` to the resulting size of the type. This essentially multiplies the size by 10 and adds 4.
 	typealias n4 = DecimalPlus1<n3>
+	/// Add the digit `5` to the resulting size of the type. This essentially multiplies the size by 10 and adds 5.
 	typealias n5 = DecimalPlus1<n4>
+	/// Add the digit `6` to the resulting size of the type. This essentially multiplies the size by 10 and adds 6.
 	typealias n6 = DecimalPlus1<n5>
+	/// Add the digit `7` to the resulting size of the type. This essentially multiplies the size by 10 and adds 7.
 	typealias n7 = DecimalPlus1<n6>
+	/// Add the digit `8` to the resulting size of the type. This essentially multiplies the size by 10 and adds 8.
 	typealias n8 = DecimalPlus1<n7>
+	/// Add the digit `9` to the resulting size of the type. This essentially multiplies the size by 10 and adds 9.
 	typealias n9 = DecimalPlus1<n8>
 }
